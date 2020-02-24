ca92156224d0b68ba3505f32be678b9b4540d31a
Deepak Pal <deepak.pal@technia.com>
2020-02-24 23:37:23 +0530

    second work on serach component

--- reactHook/src/App.js
+++ reactHook/src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { render } from "react-dom";
 import Pet from "./Pet";
+import SeSearchParams from "./searchParam";
+import SearchParams from "./searchParam";
 
 const App = () => {
   return (
     <div>
       <h1 id="some thing-important">Adopt Me !</h1>
-      <Pet name="Luna" animal="Dog" breed="Havanees" />
-      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
-      <Pet name="Doink" animal="Cat" breed="Mixed" />
+      <SearchParams></SearchParams>
     </div>
   );
 };
