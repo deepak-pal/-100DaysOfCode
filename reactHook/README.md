c148700b43143a78693aca2d391bbfd81e7ce3f2
Deepak Pal <deepak.pal@technia.com>
2020-02-25 12:03:09 +0530

    add cutomize hook

---
+++ reactHook/README.md
@@ -0,0 +1,22 @@
+# -100DaysOfCode
+I will do the coding 1 hr form 20/02/2020 100 day from now
+
+20/02/2020
+Goal 
+complete the intro to react today
+
+Hooks Best Partices
+Hook should never used with for loop and if statment as they keeping track of order 
+we can config elist so best pratice can be enfore
+command "npm i -D eslint-plugin-react-hooks" 
+update eslintrc rules as below 
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn"
+
+
+Future Task 
+How to automate Code Reviews on Github
+https://www.freecodecamp.org/news/how-to-automate-code-reviews-on-github-41be46250712/
+Code Climate
+https://www.kommunicate.io/blog/automate-code-reviews-on-github-using-a-chatbot/
+https://github.com/CocoaPods/CocoaPods/blob/master/Dangerfile
--- reactHook/src/searchParam.js
+++ reactHook/src/searchParam.js
@@ -1,24 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { ANIMALS, breeds } from "@frontendmasters/pet";
+import useDropdown from "./useDropdown";
 
 const SearchParams = () => {
-	const [ location, setLocation ] = useState('Seattle, WA');
+  const [location, setLocation] = useState("Seattle, WA");
+  const [newbreed, setBreed] = useState("");
 
-	return (
-		<div className="search-params">
-			<form>
-				<h1>{location}</h1>
-				<label htmlFor="location">
-					Location
-					<input
-						id="location"
-						value={location}
-						placeholder="Location"
-						onChange={(e) => setLocation(e.target.value)}
-					/>
-				</label>
-				<button>Submit</button>
-			</form>
-		</div>
-	);
+  const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
+  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
+
+  return (
+    <div className="search-params">
+      <form>
+        <h1>{location}</h1>
+        <label htmlFor="location">
+          Location
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={e => setLocation(e.target.value)}
+          />
+        </label>
+        <AnimalDropdown />
+        <BreedDropdown />
+      </form>
+    </div>
+  );
 };
 export default SearchParams;
