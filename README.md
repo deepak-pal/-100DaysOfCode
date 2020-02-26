d8e75d2d2a0d48b0fd2a6f17abe252928b543a2c
Deepak Pal <deepak.pal@technia.com>
2020-02-26 18:03:44 +0530

    remove readme and added to specific project;

--- README.md
+++
@@ -1,6 +0,0 @@
-# -100DaysOfCode
-I will do the coding 1 hr form 20/02/2020 100 day from now
-
-20/02/2020
-Goal 
-complete the intro to react today
--- reactHook/src/searchParam.js
+++ reactHook/src/searchParam.js
@@ -1,31 +1,30 @@
-import React, { useState } from "react";
-import { ANIMALS, breeds } from "@frontendmasters/pet";
-import useDropdown from "./useDropdown";
+import React, { useState } from 'react';
+import { ANIMALS, breeds } from '@frontendmasters/pet';
+import useDropdown from './useDropdown';
 
 const SearchParams = () => {
-  const [location, setLocation] = useState("Seattle, WA");
-  const [newbreed, setBreed] = useState("");
+	const [ location, setLocation ] = useState('Seattle, WA');
+	const [ newbreed, setBreed ] = useState('');
 
-  const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
-  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
+	const [ animal, AnimalDropdown ] = useDropdown('Animal', 'dog', ANIMALS);
+	//const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
 
-  return (
-    <div className="search-params">
-      <form>
-        <h1>{location}</h1>
-        <label htmlFor="location">
-          Location
-          <input
-            id="location"
-            value={location}
-            placeholder="Location"
-            onChange={e => setLocation(e.target.value)}
-          />
-        </label>
-        <AnimalDropdown />
-        <BreedDropdown />
-      </form>
-    </div>
-  );
+	return (
+		<div className="search-params">
+			<form>
+				<h1>{location}</h1>
+				<label htmlFor="location">
+					Location
+					<input
+						id="location"
+						value={location}
+						placeholder="Location"
+						onChange={(e) => setLocation(e.target.value)}
+					/>
+				</label>
+				<AnimalDropdown />
+			</form>
+		</div>
+	);
 };
 export default SearchParams;
